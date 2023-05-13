eea703d88f8f6eabb76a2f54bd779a4cf2d0fa26
Засимов Дмитрий <Zasimov.diman@gmail.com>
2023-05-14 02:49:10 +0400

    Седьмое задание полностью готово

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,72 +2,59 @@ class AlarmClock {
 	constructor() {
 		this.alarmCollection = [];
 		this.intervalId = null;
-		this.getCurrentFormattedTime = function() {
-			const now = new Date();
-			const hours = now.getHours().toString().padStart(2, '0');
-			const minutes = now.getMinutes().toString().padStart(2, '0');
-			return `${hours}:${minutes}`;
-		}
 	}
-
-
 
 	addClock(time, callback) {
 		if (!time || !callback) {
 			throw new Error('Отсутствуют обязательные аргументы');
 		}
-		if (this.alarmCollection.some(item => item.time === time && item.callback === callback)) {
+		if (this.alarmCollection.find(clock => clock.time === time)) {
 			console.warn('Уже присутствует звонок на это же время');
 		}
-
-		const obj = {
-			time: time,
-			callback: callback,
-			canCall: true,
-		};
-		this.alarmCollection.push(obj);
-		console.log(`Будильник на время ${time} добавлен`);
+		return this.alarmCollection.push({
+			callback,
+			time,
+			canCall: true
+		});
 	}
 
 	removeClock(time) {
-		this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
-		console.log(`Будильник на время ${time} удален`);
+		console.log(this.alarmCollection)
+		this.alarmCollection = this.alarmCollection.filter(clock => clock.time !== time);
+		console.log(this.alarmCollection)
+	}
+
+	getCurrentFormattedTime() {
+		return new Date().toTimeString().slice(0, 5);
 	}
 
 	start() {
 		if (this.intervalId) {
-			return
+			return;
 		}
-
 		this.intervalId = setInterval(() => {
-			const TimeReal = this.getCurrentFormattedTime();
-			console.log(`Текущее время: ${TimeReal}`);
-			this.alarmCollection.forEach(acc => {
-				if (acc.time === TimeReal && acc.canCall === true) {
-					acc.canCall = false;
-					acc.callback();
-					console.log(`Звонок на время ${TimeReal} выполнен`);
+			this.alarmCollection.forEach((element) => {
+				if (this.getCurrentFormattedTime() === element.time) {
+					element.canCall = false;
+					element.callback();
 				}
 			})
-		}, 1000)
+		}, 1000);
 	}
 
 	stop() {
-		if (this.intervalId) {
-			clearInterval(this.intervalId);
-			this.intervalId = null;
-			console.log('Будильник остановлен');
-		}
+		clearInterval(this.intervalId);
+		this.intervalId = null;
 	}
 
 	resetAllCalls() {
-		this.alarmCollection.forEach(acc => acc.canCall = true)
-		console.log('Все звонки будильников сброшены');
+		this.alarmCollection.forEach((element) => {
+			element.canCall = true;
+		})
 	}
 
 	clearAlarms() {
 		this.stop();
 		this.alarmCollection = [];
-		console.log('Все будильники очищены');
 	}
 }
